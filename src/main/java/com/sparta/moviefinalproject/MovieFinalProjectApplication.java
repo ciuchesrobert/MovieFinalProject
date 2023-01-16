5b54a117015bfe572d5ca602f2133eb75efefff2
rob <ciuches.robert@yahoo.com>
2023-01-16 11:28:50 +0000

    spring boot

---
+++ src/main/java/com/sparta/moviefinalproject/MovieFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.moviefinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/moviefinalproject/MovieFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.moviefinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
