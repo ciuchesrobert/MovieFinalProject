a9e8d9030523b0197ed824168e2de69524edd7cd
JB <legendineverything@gmail.com>
2023-01-16 17:36:09 +0000

    Added DAOs and DTOs and make Database objects for the additional objects in the DTOs

--- src/main/java/com/sparta/moviefinalproject/DBObjects/Critic.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.moviefinalproject.DBObjects;
-import lombok.Data;
-@Data
-public class Critic {
-    private Integer meter;
-    private Integer NumReviews;
-    private Double rating;
-
-}
