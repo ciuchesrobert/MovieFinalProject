5b2297ecd5d3f5aeb900eb8bd331a663c4c3a0e4
OGrant <OmariGrant@users.noreply.github.com>
2023-01-17 16:59:22 +0000

    Added CRUD fixes

--- src/main/java/com/sparta/moviefinalproject/restControllers/CommentController.java
+++ src/main/java/com/sparta/moviefinalproject/restControllers/CommentController.java
@@ -1,7 +1,13 @@
 package com.sparta.moviefinalproject.restControllers;
 
+import com.sparta.moviefinalproject.entities.Comment;
+import com.sparta.moviefinalproject.entities.Theater;
 import com.sparta.moviefinalproject.repositories.CommentRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.bson.types.ObjectId;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/comments/")
@@ -11,4 +17,47 @@ public class CommentController {
     public CommentController(CommentRepository commentRepository) {
         this.commentRepository = commentRepository;
     }
+
+//
+//    @GetMapping("/test")
+//    public Optional<Comment> test() {
+//        return commentRepository.findById(new ObjectId("573a1390f29313caabcd4135"));
+//    }
+
+    @GetMapping("/")
+    public Optional<Comment> findById(@RequestParam String id) {
+        return commentRepository.findById(new ObjectId(id));
+    }
+
+    @GetMapping("/all")
+    public List<Comment> findAll() {
+        return commentRepository.findAll();
+    }
+
+    @PostMapping("/create")
+    public Comment create(@RequestBody Comment comment){
+
+        return this.commentRepository.save(comment);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam String id){
+        this.commentRepository.deleteById(new ObjectId(id));
+    }
+
+    @PutMapping("/update")
+    public Comment update(@RequestBody Comment comment, @RequestParam String id) {
+        Optional<Comment> commentOptional = this.commentRepository.findById(new ObjectId(id));
+
+        if (commentOptional.isPresent()) {
+            Comment original = commentOptional.get();
+            if (comment.getText() != null) {
+                original.setText(comment.getText());
+            }
+            return this.commentRepository.save(original);
+        }
+
+        return new Comment();
+    }
+
 }
--- src/main/java/com/sparta/moviefinalproject/restControllers/UserController.java
+++ src/main/java/com/sparta/moviefinalproject/restControllers/UserController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
     final UserRepository userRepository;
 
