9931b05dc31bd79e487c1e446e45b95b2dae016a
OGrant <OmariGrant@users.noreply.github.com>
2023-01-17 11:43:00 +0000

    CRUD methods for MovieController

--- src/main/java/com/sparta/moviefinalproject/restControllers/MovieController.java
+++ src/main/java/com/sparta/moviefinalproject/restControllers/MovieController.java
@@ -4,20 +4,67 @@ package com.sparta.moviefinalproject.restControllers;
 import com.sparta.moviefinalproject.entities.Movie;
 import com.sparta.moviefinalproject.repositories.MovieRepository;
 import org.bson.types.ObjectId;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/movies/")
 public class MovieController {
     final MovieRepository movieRepository;
     public MovieController(MovieRepository movieRepository) {
         this.movieRepository = movieRepository;
     }
 
-    @GetMapping("/all")
-    public Optional<Movie> findById() {
+    @GetMapping("/test")
+    public Optional<Movie> test() {
         return movieRepository.findById(new ObjectId("573a1390f29313caabcd4135"));
     }
-}
+
+    @GetMapping("/")
+    public Optional<Movie> findById(@RequestParam String id) {
+        return movieRepository.findById(new ObjectId(id));
+    }
+
+    @GetMapping("/all")
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    @PostMapping("/create")
+    public Movie create(@RequestBody Movie movie){
+
+        return this.movieRepository.save(movie);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam String id){
+         this.movieRepository.deleteById(new ObjectId(id));
+    }
+
+    @PutMapping("/update")
+    public Movie update(@RequestBody Movie movie, @RequestParam String id) {
+        Optional<Movie> movieOptional = this.movieRepository.findById(new ObjectId(id));
+
+        if (movieOptional.isPresent()) {
+            Movie original = movieOptional.get();
+            if (movie.getYear() != 0) {
+                original.setYear(movie.getYear());
+            }
+            if(movie.getRated() != null){
+                original.setRated(movie.getRated());
+            }
+            if(movie.getTitle() != null){
+                original.setTitle(movie.getTitle());
+            }
+            if(movie.getCast() != null){
+                original.setCast(movie.getCast());
+            }
+            return this.movieRepository.save(original);
+        }
+        return new Movie();
+    }
+
+    }
