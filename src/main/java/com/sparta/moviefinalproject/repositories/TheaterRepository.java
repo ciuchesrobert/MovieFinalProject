e61c6192ed15126f5504b9c0288b7245887d66ed
OGrant <OmariGrant@users.noreply.github.com>
2023-01-17 12:08:53 +0000

    CRUD methods for Theatre. Updated TheaterRepository

--- src/main/java/com/sparta/moviefinalproject/repositories/TheaterRepository.java
+++ src/main/java/com/sparta/moviefinalproject/repositories/TheaterRepository.java
@@ -1,8 +1,9 @@
 package com.sparta.moviefinalproject.repositories;
 import com.sparta.moviefinalproject.entities.Movie;
+import com.sparta.moviefinalproject.entities.Theater;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TheaterRepository extends MongoRepository<Movie, ObjectId> {
+public interface TheaterRepository extends MongoRepository<Theater, ObjectId> {
 
 }
