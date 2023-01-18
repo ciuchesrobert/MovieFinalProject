821336b4954f4691b14a9d88c9eb2fedc60b8585
yash jagani <shi7su@protonmail.ch>
2023-01-18 12:46:58 +0000

    fixed requestmappings

--- src/main/java/com/sparta/moviefinalproject/controllers/web/CommentWebController.java
+++ src/main/java/com/sparta/moviefinalproject/controllers/web/CommentWebController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/comments/")
+@RequestMapping("/comments")
 public class CommentWebController {
 
     final CommentDAO commentDAO;
@@ -20,67 +20,67 @@ public class CommentWebController {
         this.commentDAO = commentDAO;
     }
 
-    @RequestMapping("/commentsHome")
+    @RequestMapping("/home")
     public String commentsHome(Model model){
         return "commentsHome/commentsHome";
     }
 
     // ------------- READ
-    @GetMapping("basic/search")
+    @GetMapping("/basic/search")
     public String findCommentById(Model model, ObjectId id){
         CommentDTO comment = new CommentDTO();
         model.addAttribute("comment", comment);
         return "comment/displayComment";
     }
 
-    @PostMapping("basic/search/success")
+    @PostMapping("/basic/search/success")
     public String findCommentByIdSuccess(@ModelAttribute("comment") CommentDTO comment, Model model){
         comment = commentDAO.findById( comment.getId() ).orElse(null);
         model.addAttribute("comment", comment);
         return "comment/displayCommentSuccess";
     }
 
-    @GetMapping("basic")
+    @GetMapping("/basic")
     public String getAllComments(Model model){
         List<CommentDTO> comments = commentDAO.findAll();
         model.addAttribute("comments", comments);
         return "comment/displayAllComments";
     }
 
-    @GetMapping("admin/create")
+    @GetMapping("/admin/create")
     public String createComment(Model model){
         CommentDTO comment = new CommentDTO();
         model.addAttribute("comment", comment);
         return "commentCreate";
     }
 
-    @PostMapping("admin/create/success")
+    @PostMapping("/admin/create/success")
     public String createCommentSuccess(@ModelAttribute("comment") CommentDTO comment){
         commentDAO.create(comment);
         return "commentCreateSuccess";
     }
 
-    @GetMapping("admin/update{id}")
+    @GetMapping("/admin/update{id}")
     public String updateComment(@PathVariable("id")ObjectId id, Model model){
         CommentDTO comment = commentDAO.findById(id).orElse(null);
         model.addAttribute("comment", comment);
         return "commentUpdate";
     }
 
-    @PostMapping("admin/update/success")
+    @PostMapping("/admin/update/success")
     public String updateCommentSuccess(@ModelAttribute("comment")CommentDTO comment, Model model){
         commentDAO.update(comment.getId(), comment); // - needs updating
         return "commentUpdateSuccess";
     }
 
-    @DeleteMapping("admin/delete/{id}")
+    @DeleteMapping("/admin/delete/{id}")
     public String deleteComment(@PathVariable ObjectId id, Model model){
         CommentDTO comment = commentDAO.findById(id).orElse(null);
         model.addAttribute("comment", comment);
         return "commentDelete";
     }
 
-    @PostMapping("admin/delete/success")
+    @PostMapping("/admin/delete/success")
     public String deleteCommentSuccess(@ModelAttribute("comment")CommentDTO comment, Model model){
         // check if records with given ID exists
         if( comment == null){
