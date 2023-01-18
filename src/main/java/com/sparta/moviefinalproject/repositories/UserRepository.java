578539a9060d0add3b49f16df449d25bfe07dde9
patrykblakala <patrykblakala108@gmail.com>
2023-01-18 17:44:30 +0000

    adjusting methods in userWebController to work with user templates

--- src/main/java/com/sparta/moviefinalproject/repositories/UserRepository.java
+++ src/main/java/com/sparta/moviefinalproject/repositories/UserRepository.java
@@ -5,6 +5,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, ObjectId> {
+
+    Optional<User> findByEmail(String email);
+
+    void deleteByEmail(String email);
 }
