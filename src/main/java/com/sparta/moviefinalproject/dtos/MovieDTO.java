6e13c64c4ffa945e219f4cf3d1a83e3ef6be8745
Emre <emreceyhan98@hotmail.com>
2023-01-18 16:03:34 +0000

    converters tested, equals methods between entity and dtos

--- src/main/java/com/sparta/moviefinalproject/dtos/MovieDTO.java
+++ src/main/java/com/sparta/moviefinalproject/dtos/MovieDTO.java
@@ -7,8 +7,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
-
+import com.sparta.moviefinalproject.converters.subtypes.AwardConverter;
+import com.sparta.moviefinalproject.converters.subtypes.ImdbConverter;
+import com.sparta.moviefinalproject.converters.subtypes.TomatoConverter;
 import java.time.LocalDateTime;
+import com.sparta.moviefinalproject.entities.Movie;
+import com.sparta.moviefinalproject.entities.subentities.Award;
+import com.sparta.moviefinalproject.entities.subentities.Imdb;
+import com.sparta.moviefinalproject.entities.subentities.Tomato;
 
 @Data
 @AllArgsConstructor
@@ -36,5 +42,34 @@ public class MovieDTO {
     private String[] countries;
     private String type;
 
-
+    public boolean dtoEqualsEntity(Movie obj) {
+        AwardDTO awardConverted = new AwardConverter().entityToDto(obj.getAwards());
+        TomatoDTO tomatoConverted = new TomatoConverter().entityToDto(obj.getTomatoes());
+        ImdbDTO imdbConverted = new ImdbConverter().entityToDto(obj.getImdb());
+        if (this.getId() == obj.getId() &&
+                this.getAwards().equals(awardConverted) &&
+                this.getDirectors() == obj.getDirectors() &&
+                this.getFullPlot() == obj.getFullPlot() &&
+                this.getLanguages() == obj.getLanguages() &&
+                this.getNumMflixComments() == obj.getNumMflixComments() &&
+                this.getPlot() == obj.getPlot() &&
+                this.getTitle() == obj.getTitle() &&
+                this.getWriters() == obj.getWriters() &&
+                this.getGenres() == obj.getGenres() &&
+                this.getLastUpdated() == obj.getLastUpdated() &&
+                this.getPoster() == obj.getPoster() &&
+                this.getTomatoes().equals(tomatoConverted) &&
+                this.getYear() == obj.getYear() &&
+                this.getImdb().equals(imdbConverted) &&
+                this.getRated() == obj.getRated() &&
+                this.getReleased() == obj.getReleased() &&
+                this.getCast() == obj.getCast() &&
+                this.getRuntime() == obj.getRuntime() &&
+                this.getCountries() == obj.getCountries() &&
+                this.getType() == obj.getType()){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/com/sparta/moviefinalproject/dtos/subdtos/ImdbDTO.java
+++ src/main/java/com/sparta/moviefinalproject/dtos/subdtos/ImdbDTO.java
@@ -3,7 +3,7 @@ package com.sparta.moviefinalproject.dtos.subdtos;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.util.Objects;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,4 +11,17 @@ public class ImdbDTO {
     private int id;
     private double rating;
     private int votes;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ImdbDTO imdbDto = (ImdbDTO) o;
+        return id == imdbDto.id && Double.compare(imdbDto.rating, rating) == 0 && votes == imdbDto.votes;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, rating, votes);
+    }
 }
