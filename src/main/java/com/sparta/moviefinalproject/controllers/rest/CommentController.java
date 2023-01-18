8b04c0981180454cae76e446630138eed106ee0e
brhanuh <hanibalbrhanu15@hotmail.com>
2023-01-18 16:21:58 +0000

    added apikey security

--- src/main/java/com/sparta/moviefinalproject/controllers/rest/CommentController.java
+++ src/main/java/com/sparta/moviefinalproject/controllers/rest/CommentController.java
@@ -1,11 +1,17 @@
 package com.sparta.moviefinalproject.controllers.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sparta.moviefinalproject.entities.Apikey;
 import com.sparta.moviefinalproject.entities.Comment;
 import com.sparta.moviefinalproject.repositories.ApikeyRepository;
 import com.sparta.moviefinalproject.repositories.CommentRepository;
 import org.bson.types.ObjectId;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,20 +29,49 @@ public class CommentController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Comment> findById(@PathVariable("id") String id) {
-        return commentRepository.findById(new ObjectId(id));
+    public ResponseEntity<String> findById(@PathVariable("id") String id, String apikey) throws JsonProcessingException {
+        Optional<Apikey> apikeyOptional = apikeyRepository.findByKey(apikey);
+        Apikey key = null;
+        ObjectMapper mapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        ResponseEntity<String> responseEntity = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("content-type", "application/json");
+        String commentString = null;
+        if(apikeyOptional.isPresent()){
+            key = apikeyOptional.get();
+            Comment comment = commentRepository.findById(new ObjectId(id)).get();
+            commentString = mapper.writeValueAsString(comment);
+            System.out.println(key);
+            responseEntity = new ResponseEntity<>(commentString, httpHeaders, HttpStatus.OK);
+        }else {
+            responseEntity = new ResponseEntity<>("{\"message\":\"API key " + apikey + " not valid\"}", httpHeaders, HttpStatus.NOT_FOUND);
+        }
+        return responseEntity;
     }
 
     @GetMapping
-    public List<Comment> findAll(@RequestParam String apikey) {
-        System.out.println(apikey);
+    public ResponseEntity<String> findAll(@RequestParam String apikey) throws JsonProcessingException {
         Optional<Apikey> apikeyOptional = apikeyRepository.findByKey(apikey);
         Apikey key = null;
+        ObjectMapper mapper = JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+        ResponseEntity<String> responseEntity = null;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("content-type", "application/json");
+        String commentString = null;
         if(apikeyOptional.isPresent()){
             key = apikeyOptional.get();
+            List<Comment> comments = commentRepository.findAll();
+            commentString = mapper.writeValueAsString(comments);
             System.out.println(key);
+            responseEntity = new ResponseEntity<>(commentString, httpHeaders, HttpStatus.OK);
+        }else {
+            responseEntity = new ResponseEntity<>("{\"message\":\"API key " + apikey + " not valid\"}", httpHeaders, HttpStatus.NOT_FOUND);
         }
-        return commentRepository.findAll();
+        return responseEntity;
     }
 
     @PostMapping
