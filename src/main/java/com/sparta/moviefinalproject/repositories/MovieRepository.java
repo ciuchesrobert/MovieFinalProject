48b0a3651f8d21a11a9c696a10e0c5eba018690b
CameronASHiggins <74916249+CameronASHiggins@users.noreply.github.com>
2023-01-19 12:34:10 +0000

    Changed pagination of movies to use MovieDto Tested findAllByTitleIgnoreCase in MovieTests

--- src/main/java/com/sparta/moviefinalproject/repositories/MovieRepository.java
+++ src/main/java/com/sparta/moviefinalproject/repositories/MovieRepository.java
@@ -2,6 +2,8 @@ package com.sparta.moviefinalproject.repositories;
 
 import com.sparta.moviefinalproject.entities.Movie;
 import org.bson.types.ObjectId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.List;
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
     List<Movie> findAllMoviesByTitleContainingIgnoreCase(String name);
+
+    Page<Movie> findAll(Pageable pageable);
 }
