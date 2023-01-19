799314279e18791c8e7d09f21872b9d185d688ad
brhanuh <hanibalbrhanu15@hotmail.com>
2023-01-19 11:41:41 +0000

    updated config file

--- src/main/java/com/sparta/moviefinalproject/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/moviefinalproject/config/WebSecurityConfig.java
@@ -55,6 +55,8 @@ public class WebSecurityConfig {
                     .and()
                     .rememberMe()
                     .and()
+                    .csrf()
+                    .disable()
                     .build();
         }
 
